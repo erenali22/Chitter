cceb5f3a6514e2771f6e0ca22bfd614c415700d8
erenali22 <erena.li@outlook.com>
2023-05-07 04:17:04 -0700

    chatter finished

--- react-app/src/components/Chatter/SingleChatterPage.js
+++ react-app/src/components/Chatter/SingleChatterPage.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 import ChatterItem from './ChatterItem';
 import ReplyList from '../Reply/ReplyList';
 import ReplyForm from '../Reply/ReplyForm';
@@ -6,7 +7,9 @@ import UserContext from '../../context/UserContext';
 
 const SingleChatterPage = ({ match }) => {
   const [chatter, setChatter] = useState(null);
+  const [isDataFetched, setIsDataFetched] = useState(false);
   const { user } = useContext(UserContext);
+  const history = useHistory();
 
   useEffect(() => {
     const fetchChatter = async () => {
@@ -16,15 +19,29 @@ const SingleChatterPage = ({ match }) => {
         setChatter(data);
       } catch (error) {
         console.error('Error fetching chatter:', error);
+      } finally {
+        setIsDataFetched(true);
       }
     };
 
     fetchChatter();
   }, [match.params.id]);
 
+  useEffect(() => {
+    if (isDataFetched && chatter === null) {
+      history.push('/chatters');
+    }
+  }, [chatter, history, isDataFetched]);
+
   return (
     <div>
-      {chatter && <ChatterItem chatter={chatter} currentUserId={user && user.id} />}
+      {chatter && (
+        <ChatterItem
+          chatter={chatter}
+          currentUserId={user && user.id}
+          setChatter={setChatter}
+        />
+      )}
       <ReplyList chatterId={match.params.id} />
       <ReplyForm chatterId={match.params.id} />
     </div>
