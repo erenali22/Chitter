550d1435d18129c4496e217c287bf11692f91b84
erenali22 <erena.li@outlook.com>
2023-05-15 03:52:17 -0700

    fix typo

--- react-app/src/pages/Rechatter.jsx
+++ react-app/src/pages/Rechatter.jsx
@@ -1,25 +1,31 @@
-import { useEffect } from "react"
-import MyList from "../conponents/List";
+import { useEffect } from "react";
+import MyList from "../components/List";
 
-const Rechatter = ({userInfo})=>{
-    const [chatters, setChatters] = useState([]);
-    const fetchData = async () => {
-        setChatters([])
-        try {
-          const response = await getChatters();
-          if (response.ok) {
-            const fetchedChatters = await response.json();
-            setChatters(Array.isArray(fetchedChatters) ? fetchedChatters :[]);
-          } else {
-            setError('Failed to fetch chatters');
-          }
-        } catch (err) {
-          setError('An error occurred while fetching chatters');
-        }
-      };
-    useEffect(()=>{
-        fetchData();
-    },[])
-    return <MyList userInfo={userInfo} chatters={chatters} fetchData={fetchData}></MyList>
-}
-export default Rechatter
+const Rechatter = ({ userInfo }) => {
+  const [chatters, setChatters] = useState([]);
+  const fetchData = async () => {
+    setChatters([]);
+    try {
+      const response = await getChatters();
+      if (response.ok) {
+        const fetchedChatters = await response.json();
+        setChatters(Array.isArray(fetchedChatters) ? fetchedChatters : []);
+      } else {
+        setError("Failed to fetch chatters");
+      }
+    } catch (err) {
+      setError("An error occurred while fetching chatters");
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return (
+    <MyList
+      userInfo={userInfo}
+      chatters={chatters}
+      fetchData={fetchData}
+    ></MyList>
+  );
+};
+export default Rechatter;
--- react-app/src/pages/index.jsx
+++ react-app/src/pages/index.jsx
@@ -1,19 +1,26 @@
-import { useEffect, useState } from 'react';
-import logo from '@/assets/logo.png';
-import styles from './index.module.css';
-import { LogoutOutlined, NumberOutlined, SendOutlined, ShareAltOutlined, UserOutlined } from '@ant-design/icons';
-import MyList from '@/conponents/List';
-import 'antd/dist/antd.css';
-import { Button, Tabs } from 'antd';
-import Flooter from '@/conponents/Flooter';
-import Login from '@/conponents/Login';
-import { authenticate, getChatters, newChater } from '@/api';
-import SignUp from '@/conponents/SignUp';
-import Chatter from './Chatter';
-import { getUserFeed, logout } from '../api';
-import UserList from './UserList';
-const menuList = [{ title: 'Explore', Icon: <NumberOutlined style={{ fontSize: 28 }} /> }, 
-{ title: 'Chatter', Icon: <SendOutlined style={{ fontSize: 28 }} /> }
+import { useEffect, useState } from "react";
+import logo from "@/assets/logo.png";
+import styles from "./index.module.css";
+import {
+  LogoutOutlined,
+  NumberOutlined,
+  SendOutlined,
+  ShareAltOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import MyList from "@/components/List";
+import "antd/dist/antd.css";
+import { Button, Tabs } from "antd";
+import Flooter from "@/components/Flooter";
+import Login from "@/components/Login";
+import { authenticate, getChatters, newChater } from "@/api";
+import SignUp from "@/components/SignUp";
+import Chatter from "./Chatter";
+import { getUserFeed, logout } from "../api";
+import UserList from "./UserList";
+const menuList = [
+  { title: "Explore", Icon: <NumberOutlined style={{ fontSize: 28 }} /> },
+  { title: "Chatter", Icon: <SendOutlined style={{ fontSize: 28 }} /> },
 ];
 export default function Home() {
   const [chatters, setChatters] = useState([]);
@@ -21,11 +28,9 @@ export default function Home() {
   const [isLoginOpen, setIsLoginOpen] = useState(false);
   const [isSignUpOpen, setIsSignUpOpen] = useState(false);
   const [isLoaded, setIsLoaded] = useState(false);
-  const [userInfo,setUserInfo] = useState({})
+  const [userInfo, setUserInfo] = useState({});
   const [activeTab, setActiveTab] = useState(menuList[0]?.title);
-  const handleLogin = () => {
-
-  };
+  const handleLogin = () => {};
   const openLogin = () => {
     setIsLoginOpen(true);
   };
@@ -41,120 +46,154 @@ export default function Home() {
     setIsSignUpOpen(false);
   };
   const fetchData = async () => {
-    setChatters([])
+    setChatters([]);
     try {
       const response = await getChatters();
       if (response.ok) {
         const fetchedChatters = await response.json();
-        setChatters(Array.isArray(fetchedChatters) ? fetchedChatters :[]);
+        setChatters(Array.isArray(fetchedChatters) ? fetchedChatters : []);
       } else {
-        setError('Failed to fetch chatters');
+        setError("Failed to fetch chatters");
       }
     } catch (err) {
-      setError('An error occurred while fetching chatters');
+      setError("An error occurred while fetching chatters");
     }
   };
-  const auth = ()=>{
-    authenticate().then((res)=>{
-      res.json().then((res)=>{
-        if(res?.errors){
-          setIsLoaded(false)
-        }else{
-          if(res.id){
-            setUserInfo(res)
-            setIsLoaded(true)
+  const auth = () => {
+    authenticate().then((res) => {
+      res.json().then((res) => {
+        if (res?.errors) {
+          setIsLoaded(false);
+        } else {
+          if (res.id) {
+            setUserInfo(res);
+            setIsLoaded(true);
           }
         }
-     })
-        });
-  }
+      });
+    });
+  };
   useEffect(() => {
     auth();
     fetchData();
   }, []);
   const getContent = () => {
     let contentMap = {
-      Explore: <div>
- <Tabs defaultActiveKey="1" onChange={(value)=>{
-  if(value == 1){
-    fetchData();
-  }else{
-    fetchFeedData()
-  }
- }}>
-    <Tabs.TabPane tab="For you" key="1">
-    <MyList userInfo={userInfo} chatters={chatters} fetchData={fetchData}/>
-    </Tabs.TabPane>
-    <Tabs.TabPane tab="Following" key="2">
-      <MyList userInfo={userInfo} chatters={chatters} fetchData={fetchFeedData}/>
-    </Tabs.TabPane>
-  </Tabs>
-        </div>,
-      Chatter: <Chatter />,
-      UserList:<UserList userInfo={userInfo}></UserList>
-    };
-    return contentMap[activeTab];
-  };
-  const fetchFeedData = ()=>{
-    setChatters([])
-    getUserFeed().then((fetchedChatters)=>{
-      setChatters(Array.isArray(fetchedChatters?.chatters) ? fetchedChatters?.chatters :[]);
-    })
-
-  }
-  useEffect(()=>{
-    fetchData()
-  },[activeTab])
-  return (
-    <>
-
-      <div className={styles.app}>
-        <div style={{ width: 300, paddingLeft: 10, paddingTop: 80, borderRight: '0.5px solid #ccc' }}>
-          {
-          menuList.concat(isLoaded ? [
-            { title: 'UserList', Icon: <UserOutlined style={{ fontSize: 28 }} /> },
-            { title: 'Login out', Icon: <LogoutOutlined style={{ fontSize: 28 }}/> }] : []).map((item, idx) => {
-            const { Icon } = item || {};
-          return (<div
-            style={{ display: 'flex', flexDirection: 'row', marginTop: 20, justifyContent: 'center', alignItems: 'center' }}
-            onClick={() => {
-              if(item.title === 'Login out'){
-                logout().then(()=>{
-                  auth();
-                })
-              }else{
-                setActiveTab(item.title)
+      Explore: (
+        <div>
+          <Tabs
+            defaultActiveKey="1"
+            onChange={(value) => {
+              if (value == 1) {
+                fetchData();
+              } else {
+                fetchFeedData();
               }
             }}
           >
-            <div>
-              {Icon}
-
-            </div>
-            <div style={{
-              fontSize: 26,
-              fontWeight: activeTab === item.title ? 600 : undefined,
-              marginLeft: 20,
-            }}
-            >
-              {item.title}
-            </div>
-          </div>);
-          })
-        }
+            <Tabs.TabPane tab="For you" key="1">
+              <MyList
+                userInfo={userInfo}
+                chatters={chatters}
+                fetchData={fetchData}
+              />
+            </Tabs.TabPane>
+            <Tabs.TabPane tab="Following" key="2">
+              <MyList
+                userInfo={userInfo}
+                chatters={chatters}
+                fetchData={fetchFeedData}
+              />
+            </Tabs.TabPane>
+          </Tabs>
+        </div>
+      ),
+      Chatter: <Chatter />,
+      UserList: <UserList userInfo={userInfo}></UserList>,
+    };
+    return contentMap[activeTab];
+  };
+  const fetchFeedData = () => {
+    setChatters([]);
+    getUserFeed().then((fetchedChatters) => {
+      setChatters(
+        Array.isArray(fetchedChatters?.chatters)
+          ? fetchedChatters?.chatters
+          : [],
+      );
+    });
+  };
+  useEffect(() => {
+    fetchData();
+  }, [activeTab]);
+  return (
+    <>
+      <div className={styles.app}>
+        <div
+          style={{
+            width: 300,
+            paddingLeft: 10,
+            paddingTop: 80,
+            borderRight: "0.5px solid #ccc",
+          }}
+        >
+          {menuList
+            .concat(
+              isLoaded
+                ? [
+                    {
+                      title: "UserList",
+                      Icon: <UserOutlined style={{ fontSize: 28 }} />,
+                    },
+                    {
+                      title: "Login out",
+                      Icon: <LogoutOutlined style={{ fontSize: 28 }} />,
+                    },
+                  ]
+                : [],
+            )
+            .map((item, idx) => {
+              const { Icon } = item || {};
+              return (
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    marginTop: 20,
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                  onClick={() => {
+                    if (item.title === "Login out") {
+                      logout().then(() => {
+                        auth();
+                      });
+                    } else {
+                      setActiveTab(item.title);
+                    }
+                  }}
+                >
+                  <div>{Icon}</div>
+                  <div
+                    style={{
+                      fontSize: 26,
+                      fontWeight: activeTab === item.title ? 600 : undefined,
+                      marginLeft: 20,
+                    }}
+                  >
+                    {item.title}
+                  </div>
+                </div>
+              );
+            })}
         </div>
         <div style={{ flex: 1, paddingLeft: 20, paddingTop: 60 }}>
-          <span style={{ fontSize: 30 }}>
-            {activeTab}
-          </span>
+          <span style={{ fontSize: 30 }}>{activeTab}</span>
           {getContent()}
-
         </div>
       </div>
-      {
-        !isLoaded && <Flooter openLogin={openLogin} openSignUp={openSignUp} />
-      }
-      
+      {!isLoaded && <Flooter openLogin={openLogin} openSignUp={openSignUp} />}
+
       <Login isOpen={isLoginOpen} close={closeLogin} />
       <SignUp isOpen={isSignUpOpen} close={closeSignUp} />
     </>
