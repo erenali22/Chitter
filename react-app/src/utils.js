c1b102fd57b22e59c09d11f27a2ff8447ebe0c18
erenali22 <erena.li@outlook.com>
2023-05-15 04:12:59 -0700

    update prof pic

--- react-app/src/utils.js
+++ react-app/src/utils.js
@@ -1,22 +1,13 @@
 export function formatDate(dateString) {
-    const originalDate = new Date(dateString);
-    const year = originalDate.getUTCFullYear();
-    const month = String(originalDate.getUTCMonth() + 1).padStart(2, '0');
-    const day = String(originalDate.getUTCDate()).padStart(2, '0');
+  const originalDate = new Date(dateString);
+  const year = originalDate.getUTCFullYear();
+  const month = String(originalDate.getUTCMonth() + 1).padStart(2, '0');
+  const day = String(originalDate.getUTCDate()).padStart(2, '0');
 
-    const formattedDate = `${year}-${month}-${day}`;
-    return formattedDate;
-  }
-  const profilePictures = [
-    'https://pbs.twimg.com/profile_images/1590968738358079488/IY9Gx6Ok_x96.jpg',
-    'https://pbs.twimg.com/profile_images/1086020588051857408/TyFdPl03_x96.jpg',
-    'https://pbs.twimg.com/profile_images/1638827572938022914/eqB5CGBN_x96.jpg',
-    'https://pbs.twimg.com/profile_images/1636443983990669326/xm-YehdR_x96.jpg',
-    'https://pbs.twimg.com/profile_images/1568417445211152388/YsUHG-Ea_x96.jpg',
-    'https://pbs.twimg.com/profile_images/1086020588051857408/TyFdPl03_x96.jpg',
-  ];
+  const formattedDate = `${year}-${month}-${day}`;
+  return formattedDate;
+}
 
-  export function getRandomProfilePicture() {
-    const randomIndex = Math.floor(Math.random() * profilePictures.length);
-    return profilePictures[randomIndex];
-  }
+export function getRandomProfilePicture() {
+  return 'https://cataas.com/cat?width=96&height=96&s=' + Math.random();
+}
